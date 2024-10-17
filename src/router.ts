052ac3456cb133b1708e0adb6e86dd5e1238a5df
Dr-Greg <grigor.mouradyan@outlook.fr>
2024-10-17 23:27:36 +0200

    fix: health route

--- src/router.ts
+++ src/router.ts
@@ -2,9 +2,11 @@ import { Router } from "@oak/oak/router";
 import { getAll } from "./controllers/get-items.ts";
 import type { Context } from "@oak/oak";
 
-const router = new Router({prefix: "/tarkov-cc-api"});
+const router = new Router({ prefix: "/tarkov-cc-api" });
 
 router.get("/", getAll);
-router.get("/health", (ctx: Context) => {ctx.response.body = "ok"});
+router.get("/health", (ctx: Context) => {
+  ctx.response.body = { health: "OK" };
+});
 
 export default router;
