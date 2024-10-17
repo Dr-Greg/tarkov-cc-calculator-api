17e83e67cbfb4fb20ef85f369cf72de4621fca11
Dr-Greg <grigor.mouradyan@outlook.fr>
2024-10-17 20:21:38 +0200

    refactor: remove clg

--- src/controllers/get-items.ts
+++ src/controllers/get-items.ts
@@ -4,7 +4,6 @@ import type { Context } from "@oak/oak";
 async function getAll(ctx: Context) {
   try {
     const items = await database.fetchAll<Item>("items");
-    console.log(items);
     ctx.response.body = items;
   } catch (err) {
     console.log(err);
