c802ee1ae92843266eb1c52b9891eca51d4e3647
Dr-Greg <grigor.mouradyan@outlook.fr>
2024-10-21 12:54:59 +0200

    refactor: use dao to fetch items

--- src/controllers/get-items.ts
+++ src/controllers/get-items.ts
@@ -1,13 +1,11 @@
-import database from "../services/database.ts";
 import type { Context } from "@oak/oak";
+import Item from "../daos/Item.ts";
 
-async function getAll(ctx: Context) {
+export default async function (ctx: Context) {
   try {
-    const items = await database.fetchAll<Item>("items");
+    const items = await Item.find();
     ctx.response.body = items;
   } catch (err) {
     console.log(err);
   }
 }
-
-export { getAll };
