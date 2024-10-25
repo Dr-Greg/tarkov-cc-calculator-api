7068c3076d9ee8102db9ea6089896b3328fa2adb
Dr-Greg <grigor.mouradyan@outlook.fr>
2024-10-25 18:35:43 +0200

    refactor: handle body parsing error

--- src/controllers/get-items-with-category.ts
+++ src/controllers/get-items-with-category.ts
@@ -1,23 +1,25 @@
 import type { Context } from "@oak/oak";
 import Item from "../daos/Item.ts";
 
-export default async function (ctx: Context) {
+export default async function (ctx: Context<{ body: Array<string> }>) {
   try {
-    if (!ctx.request.hasBody) {
-      ctx.response.status = 400;
-      ctx.response.body = { message: "no body" };
-      return;
-    }
-
     const types = await ctx.request.body.json();
     if (!types || !types.length) {
       ctx.response.status = 400;
-      ctx.response.body = { message: "no types" };
+      ctx.response.body = {
+        error: "Invalid threshold provided",
+        message: "Threshold must be a valid number",
+      };
       return;
     }
     const items = await Item.find({ types: { $in: types } });
     ctx.response.body = items;
   } catch (err) {
+    if (String(err).includes("BadRequestError")) {
+      ctx.response.status = 400;
+      ctx.response.body = { message: "invalid body" };
+      return;
+    }
     console.log(err);
     ctx.response.status = 500;
   }
