c7930427267ace32663c8476dcb73971abc18831
Dr-Greg <grigor.mouradyan@outlook.fr>
2024-10-25 18:30:14 +0200

    chore: add all types and default types arrays for items

---
+++ src/constants/item_types.ts
@@ -0,0 +1,19 @@
+const ALL_TYPES = Object.freeze([
+    "barter",
+    "container",
+    "glasses",
+    "grenade",
+    "headphones",
+    "injectors",
+    "mods",
+    "pistolGrip",
+    "suppressor",
+]);
+
+const DEFAULT_TYPES = Object.freeze([
+    "barter",
+    "container",
+    "suppressor",
+]);
+
+export { ALL_TYPES, DEFAULT_TYPES };
