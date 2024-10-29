34c71dee8b2e3e49c4cbbf1f67c41ebf67e8d00e
Dr-Greg <grigor.mouradyan@outlook.fr>
2024-10-29 19:20:58 +0100

    feat: add POST /best-item-combo routewith body validation

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
 import type { Context } from "@oak/oak";
 import { Router } from "@oak/oak/router";
-import getAllItems from "./controllers/get-items.ts";
-import getItemsWithCategory from "./controllers/get-items-with-category.ts";
 import getBestItemCombo from "./controllers/get-best-item-combo.ts";
+import getItemsWithCategory from "./controllers/get-items-with-category.ts";
+import getAllItems from "./controllers/get-items.ts";
+import { validateBody } from "./middlewares/handlers/body-validation.handler.ts";
+import bestItemComboSchema from "./schemas/best-item-combo.schema.ts";
 
 const router = new Router({ prefix: "/tarkov-cc-api" });
 
@@ -10,8 +12,12 @@ router.get("/health", (ctx: Context) => {
   ctx.response.body = { health: "OK" };
 });
 
-router.get("/", getAllItems).post("/", getItemsWithCategory).post(
+router.get("/", getAllItems).post(
+  "/",
+  getItemsWithCategory,
+).post(
   "/best-item-combo",
+  validateBody(bestItemComboSchema),
   getBestItemCombo,
 );
 
