f73a9aa4ce51a45b81a6fe18ae665e7612596f68
Dr-Greg <grigor.mouradyan@outlook.fr>
2024-10-30 19:20:26 +0100

    feat: apply http logger middleware to app

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import { Application } from "@oak/oak";
 
 import { config } from "./config.ts";
 import errorHandler from "./middlewares/handlers/error.handler.ts";
+import loggerMiddleware from "./middlewares/logger.middleware.ts";
 import router from "./router.ts";
 
 const app = new Application();
 
 app.use(errorHandler);
+app.use(loggerMiddleware);
 
 app.use(router.routes());
 app.use(router.allowedMethods());
